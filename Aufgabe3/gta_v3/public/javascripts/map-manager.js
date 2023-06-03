fe2e61f67086c027e8772fab627c85126cbf3f53
blazeit420 <61168939+con-gif@users.noreply.github.com>
2023-06-03 12:43:03 +0200

    2. TA Teil A erledigt

--- Aufgabe3/gta_v3/public/javascripts/map-manager.js
+++ Aufgabe3/gta_v3/public/javascripts/map-manager.js
@@ -1,14 +1,12 @@
-// File origin: VS1LAB A2 
-
 /**
  * A class to help using the MapQuest map service.
  */
-// eslint-disable-next-line no-unused-vars
 class MapManager {
-    #apiKey
+    /* besorgt am 22.04.2023*/
+    #apiKey = 'HltU54BEnKkv8JTPR4OPpEmjzjtUNOTs';
 
     /**
-     * Create a new MapManager instance
+     * Create a new MapManager instance.
      * @param {string} apiKey Your MapQuest API Key
      */
     constructor(apiKey) {
@@ -16,21 +14,21 @@ class MapManager {
     }
 
     /**
-     * Generate a MapQuest image URL for the specified parameters
+     * Generate a MapQuest image URL for the specified parameters.
      * @param {number} latitude The map center latitude
      * @param {number} longitude The map center longitude
      * @param {{latitude, longitude, name}[]} tags The map tags, defaults to just the current location
-     * @param {number} zoom The map zoom, defaults to 11
+     * @param {number} zoom The map zoom, defaults to 10
      * @returns {string} URL of generated map
      */
-    getMapUrl(latitude, longitude, tags = [], zoom = 11) {
-        if (!this.#apiKey) {
+    getMapUrl(latitude, longitude, tags = [], zoom = 10) {
+        if (this.#apiKey === '') {
             console.log("No API key provided.");
             return "images/mapview.jpg";
         }
 
         let tagList = `${latitude},${longitude}|marker-start`;
-        tagList += tags.reduce((acc, tag) => `${acc}||${tag.latitude},${tag.longitude}|flag-${tag.name}`, "");
+        tagList += tags.reduce((acc, tag) => `${acc}||${tag.latitude},${tag.longitude}|flag-${tag.name_hidden}`, "");
 
         const mapQuestUrl = `https://www.mapquestapi.com/staticmap/v5/map?key=${this.#apiKey}&size=600,400&zoom=${zoom}&center=${latitude},${longitude}&locations=${tagList}`;
         console.log("Generated MapQuest URL:", mapQuestUrl);
